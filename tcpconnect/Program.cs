2063c04f7353cfca0f983ccf087f5841f5df97d4
kazuma.omachi <kaz.00.machi@gmail.com>
2017-10-08 12:49:44 +0900

    1st commit

---
+++ tcpconnect/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace tcpconnect
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			//サーバーのIPアドレス（または、ホスト名）とポート番号
+			string ipOrHost = "127.0.0.1";
+			//string ipOrHost = "localhost";
+			int port = 2001;
+
+			//TcpClientを作成し、サーバーと接続する
+			System.Net.Sockets.TcpClient tcp = new System.Net.Sockets.TcpClient(ipOrHost, port);
+		}
+	}
+}
---
+++ testTcpComunication/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace testTcpComunication
+{
+	class MainClass
+	{
+		public static System.Net.Sockets.TcpListener listener;
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			string ipString = "127.0.0.1";
+			System.Net.IPAddress ipAdd = System.Net.IPAddress.Parse(ipString);
+
+			//Listenするポート番号
+			int port = 2001;
+
+			//TcpListenerオブジェクトを作成する
+			listener = new System.Net.Sockets.TcpListener(ipAdd, port);
+
+			//Listenを開始する
+			listener.Start();
+
+			System.Threading.ThreadPool.QueueUserWorkItem(acceptclient);
+
+			while (true) { }
+
+		}
+
+		public static void acceptclient(object e)
+		{
+			System.Net.Sockets.TcpClient client = listener.AcceptTcpClient();
+			System.Threading.ThreadPool.QueueUserWorkItem(acceptclient);
+
+			System.Net.Sockets.NetworkStream ns = client.GetStream();
+			Console.WriteLine("connect");
+		}
+	}
+}
